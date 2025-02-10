3ca58afe57cc33a9c451c037c394397cd735a072
Matheus Torreão <mtorreao1@gmail.com>
2025-02-10 14:02:44 -0300

    .

--- db/migrate/20250210162819_todo_list_has_one_user.rb
+++ db/migrate/20250210162819_todo_list_has_one_user.rb
@@ -1,6 +1,6 @@
 class TodoListHasOneUser < ActiveRecord::Migration[8.0]
   def change
-    add_reference :todo_lists, :user, null: false, foreign_key: true
-    add_column :todo_items, :user_id, :bigint
+    add_reference :todo_lists, :user, null: true, foreign_key: true
+    add_column :todo_items, :user_id, :bigint, null: true
   end
 end
--- db/migrate/20250210165726_change_column_user_id_to_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnUserIdToDefault < ActiveRecord::Migration[8.0]
-  def change
-    change_column :todo_items, :user_id, :bigint, default: nil
-  end
-end
